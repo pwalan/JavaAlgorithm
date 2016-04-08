01c4be3d876a6e5ba35f2a61dd1d7b268e4a82f5
pwalan <pwalan@qq.com>
2016-04-08 11:52:11 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int i=0;
+        double[] xy=new double[10];
+        Scanner sc=new Scanner(System.in);
+
+        while(sc.hasNext()){ 
+            xy[i]=sc.nextDouble();
+            i++;
+            if(4==i){
+                System.out.println(String.format("%.2f", Math.sqrt((xy[0]-xy[2])*(xy[0]-xy[2])+(xy[1]-xy[3])*(xy[1]-xy[3]))));
+                i=0;
+            }
+        }
+    }
+}
