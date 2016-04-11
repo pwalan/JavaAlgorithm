5293ac36dc6dbb39a7607ef88ad3d85a510604e9
pwalan <pwalan@qq.com>
2016-04-11 21:44:12 +0800

    bestcoder2005

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import java.io.*;
+import java.util.Date;
 import java.util.Scanner;
-import java.lang.Math;
+import java.text.SimpleDateFormat;
 
 /**
- * http://acm.acmcoder.com/showproblem.php?pid=2010 
+ * http://acm.acmcoder.com/showproblem.php?pid=2005 
  * @author AlanP
  * 
  */
@@ -12,33 +12,10 @@ public class Main {
 	
     public static void main(String[] args){
     	Scanner sc=new Scanner(System.in);
-    	
-    	int m,n;
-    	boolean exist;
-    	boolean isfirst;
-
-        while(sc.hasNext()){
-        	m=sc.nextInt();
-        	n=sc.nextInt();
-        	exist=false;
-        	isfirst=true;
-        	for(int i=m;i<=n;i++){
-        		if(i==(i/100)*(i/100)*(i/100)+(i/10%10)*(i/10%10)*(i/10%10)+(i%10)*(i%10)*(i%10)){
-        			if(isfirst){
-        				System.out.print(i);
-        				isfirst=false;
-        			}else{
-        				System.out.print(" "+i);  //注意此处的输出控制，一旦把握不好就会出现Presentation Error
-        			}
-        			exist=true;
-        		}
-        	}
-        	if(!exist){
-        		System.out.println("no");
-        	}else{
-        		System.out.println("");
-        	}
-        	
+    	String date;
+    	SimpleDateFormat sdf=new SimpleDateFormat("D");
+        while(sc.hasNextLine()){
+        	System.out.println(sdf.format(new Date(sc.nextLine())));
         }  //The end of while.
     }  //The end of main function.
 }  //The end of Main class.
