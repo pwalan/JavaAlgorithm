8006fd17f4dc0591ed280cbadd99744d2118b83a
pwalan <pwalan@qq.com>
2016-04-22 10:58:38 +0800

    Baidu 2016 trainee recruitment exam online, second programming problem.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,41 @@
-import java.util.Date;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-import java.text.SimpleDateFormat;
 
 /**
- * http://acm.acmcoder.com/showproblem.php?pid=2005 
+ * Baidu 2016 trainee recruitment exam online, second programming problem.
  * @author AlanP
  * 
  */
 
 public class Main {
-	
     public static void main(String[] args){
+    	int n,m,k;
+    	int i,j;
     	Scanner sc=new Scanner(System.in);
-    	String date;
-    	SimpleDateFormat sdf=new SimpleDateFormat("D");
-        while(sc.hasNextLine()){
-        	System.out.println(sdf.format(new Date(sc.nextLine())));
+        while(sc.hasNextInt()){
+        	n=sc.nextInt();
+        	m=sc.nextInt();
+        	k=sc.nextInt();
+        	ArrayList arr=new ArrayList();
+        	i=1;
+        	j=1;
+        	while(i<=n){
+        		while(j<=m){
+        			if(i*j<=k){
+        				arr.add(i*j);
+        				j++;
+        			}else{    				
+        				break;
+        			}
+        		}
+        		j=1;
+        		i++;
+        	}
+        	Collections.sort(arr);
+        	System.out.println(arr);
+        	System.out.println(arr.get(k-1));
         }  //The end of while.
     }  //The end of main function.
 }  //The end of Main class.
