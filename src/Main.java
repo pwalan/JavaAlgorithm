49c13aac80aceef6be7b55cc480ba350936af0f7
pwalan <pwalan@qq.com>
2016-05-05 19:44:00 +0800

    some change

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,32 @@ import java.util.Collections;
 import java.util.Scanner;
 
 /**
- * Baidu 2016 trainee recruitment exam online, second programming problem.
+ * Algorithms Fourth Edition 1.1.18
  * @author AlanP
  * 
  */
 
 public class Main {
+	 public static int mystery1(int a, int b)
+	    {
+	        StdOut.printf("%3d, %3d\n", a, b);
+	        if (b == 0) return 0;
+	        if (b % 2 == 0) return mystery1(a+a, b/2);
+	        return mystery1(a+a, b/2) + a;
+	    }
+	    
+	    public static int mystery2(int a, int b)
+	    {
+	        StdOut.printf("%7d, %3d\n", a, b);
+	        if (b == 0) return 1;
+	        if (b % 2 == 0) return mystery2(a*a, b/2);
+	        return mystery2(a*a, b/2) * a;
+	    }
+	    
     public static void main(String[] args){
-    	int n,m,k;
-    	int i,j;
-    	Scanner sc=new Scanner(System.in);
-        while(sc.hasNextInt()){
-        	n=sc.nextInt();
-        	m=sc.nextInt();
-        	k=sc.nextInt();
-        	ArrayList arr=new ArrayList();
-        	i=1;
-        	j=1;
-        	while(i<=n){
-        		while(j<=m){
-        			if(i*j<=k){
-        				arr.add(i*j);
-        				j++;
-        			}else{    				
-        				break;
-        			}
-        		}
-        		j=1;
-        		i++;
-        	}
-        	Collections.sort(arr);
-        	System.out.println(arr);
-        	System.out.println(arr.get(k-1));
-        }  //The end of while.
-    }  //The end of main function.
+    	 StdOut.println("Result: " + mystery1(5, 7));
+         StdOut.println();
+         StdOut.println("Result: " + mystery2(5, 7));
+         
+   }  //The end of main function.
 }  //The end of Main class.
